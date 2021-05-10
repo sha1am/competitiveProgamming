8ef108eb1773ea65269f2480a112ac3f1021c68a
shaaalaaam <shaaalaaam@gmail.com>
2021-05-10 20:43:50 +0530

    up

---
+++ CFlongestregularBracketcpp.cpp
@@ -0,0 +1,115 @@
+#include "bits/stdc++.h"
+using namespace std;
+//#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+ 
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+ 
+const int N=0;
+ 
+
+ 
+void solve(){
+	string str;
+	cin>>str;
+	int strSize=str.length();
+	vector<int> d(strSize,-2),c(strSize,-2),f(strSize+1,0);
+	stack<int> stk;
+
+	for(int i=0;i<strSize;i++){
+		if(str[i]=='('){
+			stk.push(i);
+		}
+		else{
+			if(!stk.empty()){
+				d[i]=c[i]=stk.top();
+				stk.pop();
+				if(str[d[i]-1]==')' && c[d[i]-1]!=-1){
+					c[i]=c[d[i]-1];
+				}
+				int length=i-c[i]+1;
+				f[length]+=1;
+			}
+			else{
+				d[i]=c[i]=-1;
+			}
+		}
+	}
+	debug(c);
+	f[0]=1;
+	debug(f);
+	for( int i=strSize;i>=0;i--){
+		if(f[i]>0){
+			cout<<i<<" "<<f[i]<<endl;
+			break;
+		}
+	}
+
+}
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	freopen("input.in", "r", stdin);
+	freopen("output.in", "w", stdout);
+	#ifndef ONLINE_JUDGE
+	freopen("error.in", "w", stderr);
+    #endif
+	#ifdef SIEVE
+		sieve();
+	#endif
+	#ifdef NCR
+		init();
+	#endif
+	
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
---
+++ jobSequencingGreedy.cpp
@@ -0,0 +1,108 @@
+#include "bits/stdc++.h"
+using namespace std;
+//#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+ 
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+ 
+const int N=0;
+ 
+ struct job{
+ 	double profit, deadline;
+ 	job(double profit, double deadline){
+ 		this->profit=profit;
+ 		this->deadline=deadline;
+ 	}
+ };
+bool cmp(struct job j1, struct job j2){
+	return j1.profit>j2.profit;
+}
+ double jobSequencing(job arr[],int n){
+ 	sort( arr,arr+n,cmp);
+ 	int vecSize=arr[0].profit;
+ 	vector<double> vec(arr[0].profit,0);
+ 	double totProfit=0;
+ 	for(int i=0;i<n;i++){
+ 		for(int j=arr[i].deadline-1;j>=0;j--){
+ 			if(vec[j]==0){
+ 				vec[j]=1;
+ 				totProfit+=arr[i].profit;
+ 				break;
+ 			}
+ 		}
+ 	}
+ 	return totProfit;
+ }
+ 
+void solve(){
+	job arr[]={{35,3},{30,4},{25,4},{20,2},{15,3},{12,1},{5,2}};
+	int n= sizeof(arr)/sizeof(arr[0]);
+	cout<<"the maximum profit that can be earned is "<<jobSequencing(arr,n)<<endl;
+}
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	freopen("input.in", "r", stdin);
+	freopen("output.in", "w", stdout);
+	#ifndef ONLINE_JUDGE
+	freopen("error.in", "w", stderr);
+    #endif
+	#ifdef SIEVE
+		sieve();
+	#endif
+	#ifdef NCR
+		init();
+	#endif
+	
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
