052d90b8f44006e349fc2d96524e02ca7bb141f2
shaaalaaam <shaaalaaam@gmail.com>
2022-05-05 23:52:52 +0530

    update

---
+++ NOTcp/FlutterPlayground/dart/aam.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'dart:core';
+
+void main() {
+  int a = 1010;
+  int b = 200;
+  int c = a + b;
+  print("The value of c is ::1000 ");
+  print(a);
+} 
+// export PATH=$PATH:/usr/local/Caskroom/flutter/2.10.5/flutter/bin/cache/dart-sdk/bin
--- Topics /Codeforces/aamcf.cpp
+++ Topics /Codeforces/aamcf.cpp
@@ -1,147 +1,29 @@
-#include "bits/stdc++.h"
-using namespace std;
-#define int               long long
-#define pb                push_back
-#define ppb               pop_back
-#define pf                push_front
-#define ppf               pop_front
-#define all(x)            (x).begin(),(x).end()
-#define uniq(v)           (v).erase(unique(all(v)),(v).end())
-#define sz(x)             (int)((x).size())
-#define fr                first
-#define sc                second
-#define pii               pair<int,int>
-#define rep(i,a,b)        for(int i=a;i<b;i++)
-#define mem1(a)           memset(a,-1,sizeof(a))
-#define mem0(a)           memset(a,0,sizeof(a))
-#define ppc               __builtin_popcount
-#define ppcll             __builtin_popcountll
-
-//Modified
-#define time(s)       (double(clock()-s)/double(CLOCKS_PER_SEC))
-#define lcm(a, b)      (a * (b / __gcd(a,b)))
-#define endl			"\n"
-
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-//clock
-clock_t start;
-// mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
-
-void _print(long long t) {cerr << t;}
-void _print(string t) {cerr << t;}
-//void _print(int t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(long double t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(unsigned long long t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(deque<T> d);
-template <class T> void _print(queue<T> q);
-template <class T> void _print(stack<T> s);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(deque<T> d) {cerr <<"["; while(!d.empty()) {_print(d.front()); cerr<<" "; d.pop_front();} cerr<< "]"; }
-template <class T> void _print(queue<T> q) {cerr <<"["; while(!q.empty()) {_print(q.front()); cerr<<" "; q.pop();} cerr<< "]"; }
-template <class T> void _print(stack<T> s) {cerr <<"["; while(!s.empty()) {_print(s.top()); cerr<<" "; s.pop();} cerr <<"]";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a) {in>>a.fr>>a.sc; return in;}
-template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a) {out<<a.fr<<" "<<a.sc; return out;}
-template<typename T,typename T1>T amax(T &a,T1 b) {if(b>a)a=b; return a;}
-template<typename T,typename T1>T amin(T &a,T1 b) {if(b<a)a=b; return a;}
-
-const long long INF=1e18;
-const long long minINF=-1e18;
-const int32_t M=1e9+7;
-const int32_t MM=998244353;
-int N=100002;
-
-void solve() {
-	int n;
-	cin>>n;
-
-	vector<int> v(n,-1);
-
-	map<int,int> mp;
-	rep(i,0,n) {
-		cin>>v[i];
-		if(mp.find(v[i])!=mp.end()) {
-			mp[v[i]]+=1;
-		}
-		else{
-			mp[v[i]]=1;
-		}
-	}
-	
-	debug(mp);
-	
-	//find max in map?
-	
-	map<int,int> ::iterator it;
-	it=mp.begin();
-	
-	int maxx=minINF;
-	for(;it!=mp.end();it++){
-		if(it->second > maxx) maxx=it->second;
-	}
-	
-	debug(maxx);
-	
-	int ans=-1;
-	if(maxx==n){
-		ans=0;
-	}
-	else{
-		
-		int cnt=0;
-		while(maxx<n){
-			cnt+=maxx;
-			maxx*=2;
-		}
-		
-		ans=cnt-1;
-	}
-	
-	
-	cout<<ans<<endl; 
-
-
+01.	#include <iostream>
+02.	using namespace std;
+03.	string getDivision(float avgM) {
+	04.	if (avgM >= 75) return "First Division with distinction";
+	05.	if (avgM >= 60) return "First division";
+	06.	if (avgM >= 50) return "Second division";
+	07.	if (avgM >= 40) return "Third division";
+	08.	if (avgM >= 0) return "Fail";
+	09.	return "ERROR";
+	10.
 }
-
-signed main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	// freopen("input.in", "r", stdin);
-	// freopen("output.in", "w", stdout);
-	start = clock();
-#ifndef ONLINE_JUDGE
-	freopen("error.in", "w", stderr);
-#endif
-
-#ifdef SIEVE
-	sieve();
-#endif
-#ifdef NCRk
-	init();
-#endif
-	cout << fixed << setprecision(12);
-
-	int t=1;
-	cin>>t;
-	while(t--) solve();
-	cerr <<"Time Taken: "<<time(start);
-	return 0;
+11.	bool chkVal(int mark) {
+	12.	if(mark <= 100) if(mark >= 0) return true;
+	13.	return false;
+	14.
+}
+15.	int main() {
+	16.	int marks[3];
+	17.	cin >> marks[0] >> marks[1] >> marks[2];
+	18.	if(chkVal(marks[0]) && chkVal(marks[1]) && chkVal(marks[2]))
+		19.	{
+		20.	float avrg = (marks[0] + marks[1] + marks[2]) / 3;
+		21.	cout << getDivision(avrg) << endl;
+		22.	} else {
+		23.	cout << "ERROR" << endl;
+		24.
+	}
+	25.
 }
