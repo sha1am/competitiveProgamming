2f66c62ca264e04579216b601c97686a08b6ad9e
shaaalaaam <shaaalaaam@gmail.com>
2022-05-26 18:41:06 +0530

    update

--- NOTcp/FlutterPlayground/dart/aam.dart
+++ NOTcp/FlutterPlayground/dart/aam.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 import 'dart:core';
 
 void main() {
-  int a = 1010;
+  int a = 1012;
   int b = 200;
   int c = a + b;
-  print("The value of c is ::1000 ");
+  print("The value of c is :: $c ");
   print(a);
 } 
 // export PATH=$PATH:/usr/local/Caskroom/flutter/2.10.5/flutter/bin/cache/dart-sdk/bin
+
+
